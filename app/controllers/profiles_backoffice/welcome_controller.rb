c23b0a3e099b4094aa571683db2ae81bdb33e049
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-20 17:47:12 -0300

    redefine controller profile for user

--- app/controllers/profiles_backoffice/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class ProfilesBackoffice::WelcomeController < ProfilesBackofficeController
-# Este controller herda do ProfilesBackofficeController onde está definido o layout da view
-  def index
-  end
-end
